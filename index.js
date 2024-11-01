4ec28f64fe3dadee1417ecdc3c26a31effc92df5
0wlboy <35744878+0wlboy@users.noreply.github.com>
2024-10-31 20:24:43 -0400

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+/*for(let x =1;x<=100;x++){
+  console.log(x)
+}*/
+
+/*for(let x = 0; x<=100;x++){
+  if (x%2===0){
+    console.log(x)
+  }
+}*/
+
+/*let fib =[]
+for (let x = 0; x<=15;x++){
+  if(x>=2){
+    fib.push(fib[x-1] + fib[x-2]);
+  }else{
+    fib.push(x)
+  }
+  console.log(fib[x])
+}*/
+
+
